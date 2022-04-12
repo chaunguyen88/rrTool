56cef9971dbbf58775bbfcca2620332e9e90a105
chaunguyen88 <caoducchau@gmail.com>
2022-04-12 11:37:46 +0700

    Add files via upload

---
+++ AdWebLoginApi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using Flurl.Http;
+
+namespace Nop.Web
+{
+    public class AdWebLoginApi
+    {
+        public String getAccessToken(string client_id, string redirect_uri, string client_secret, string code)
+        {
+            string url = "http://idmgt.fushan.fihnbb.com:8069/adweb/oauth2/access_token/v1";
+            string postData = "client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&client_secret=" + client_secret + "&code=" + code + "&grant_type=authorization_code";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return responseString;
+        }
+
+        public string getUserInfo(string access_token)
+        {
+            var responseString = "http://idmgt.fushan.fihnbb.com:8069/adweb/people/me/v1"
+                .WithOAuthBearerToken(access_token)
+                .GetStringAsync().Result;
+            return responseString;
+        }
+
+
+        public string getUserDetail(string access_token)
+        {
+            var responseString = "http://idmgt.fushan.fihnbb.com:8069/adweb/record/search/v1?model=hr.employee&search_datas=[(\"ad_user_sAMAccountName\",\"ilike\",\"tngthan1\")]&fields=[\"name\",\"id\"]"
+                .WithOAuthBearerToken(access_token)
+                .GetStringAsync().Result;
+            return responseString;
+        }
+
+        public string getGroupByUser(string access_token, string fushan_account)
+        {
+            var responseString = ("http://idmgt.fushan.fihnbb.com/adweb/record/search/v1?model=fih.nbb.group&search_datas=[(\"employee_ids.ad_user_sAMAccountName\",\"in\",['" + fushan_account + "'])]&fields=[\"name\",\"id\"]")
+                 .WithOAuthBearerToken(access_token)
+                 .GetStringAsync().Result;
+            return responseString;
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Security.Principal;
+using System.Web.Security;
+
+using Nop.Core;
+using Nop.Core.Data;
+using Nop.Core.Infrastructure;
+using System.Data.Entity;
+using Nop.Data;
+
+namespace Nop.Web
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Khởi tạo ứng dụng: Đăng ký các dịch vụ sẽ sử dụng trên hệ thống
+        /// </summary>
+        protected void Application_Start()
+        {
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+
+            // Database.SetInitializer<NopObjectContext>(new DropCreateDatabaseAlways<NopObjectContext>());
+            //initialize engine context
+            EngineContext.Initialize(false);
+        }
+
+
+
+        /// <summary>
+        /// Khởi tạo phiên làm việc mới
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session[SessionConst.SESSION_START] = Guid.NewGuid().ToString();
+        }
+
+
+        /// <summary>
+        /// Tắt phiên làm việc
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Session.Remove(SessionConst.SESSION_START);
+        }
+    }
+}
